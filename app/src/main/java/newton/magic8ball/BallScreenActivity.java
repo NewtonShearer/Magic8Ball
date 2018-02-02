120cbb4228334093da865cbcdab29e4927823f17
NewtonShearer <laglacesoi@gmail.com>
2018-02-02 12:30:41 -0600

    Cleanup and final code testing.

--- app/src/main/java/newton/magic8ball/BallScreenActivity.java
+++ app/src/main/java/newton/magic8ball/BallScreenActivity.java
@@ -1,6 +1,9 @@
 package newton.magic8ball;
 
 import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Vibrator;
@@ -17,6 +20,9 @@ import java.util.Random;
  */
 
 public class BallScreenActivity extends AppCompatActivity {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,44 +30,67 @@ public class BallScreenActivity extends AppCompatActivity {
         setContentView(R.layout.ball_screen);
         rollEvent();
 
-
-
+        //Setup for the phone listening for shake events.
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake(int count) {
+                reShakeEvent();
+            }
+        });
     }
 
+    //Stop listening for shake events, saves battery.
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
+    //Resume listening for shake events.
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(mShakeDetector,mAccelerometer,SensorManager.SENSOR_DELAY_UI);
+        super.onResume();
+    }
+
+    //User clicks the return button
     public void returnButtonClick(View v) {
         finish();
     }
 
-
+    //Method for loading the answer from a question.
     public void rollEvent() {
+        //imageview setup
         ImageView fadeImage = (ImageView)findViewById(R.id.imageView);
-
-
-
-        //Roll random between 0-19
-        int random = new Random().nextInt(20) + 0;
-        int testRandom = 5;
-
+        //Roll random between 0-20
+        int random = new Random().nextInt(21) + 0;
+        //Setup for altering the image of the answer.
         Context c = getApplicationContext();
-        int id = c.getResources().getIdentifier("drawable/"+"test"+String.valueOf(testRandom),null,c.getPackageName());
-
+        //Change the image based on what the random number was
+        int id = c.getResources().getIdentifier("drawable/"+"t"+String.valueOf(random),null,c.getPackageName());
+        //Start fade animation
         fadeImage.setImageResource(id);
         Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
         fadeImage.startAnimation(fadeAnimation);
-
-
-
         //vibrate phone while activity resets
         shakeVibrationEvent();
-
-
     }
 
+    //Method for vibrating the phone when the user shakes it.
     public void shakeVibrationEvent() {
         Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         // Vibrate for 500 milliseconds
         v.vibrate(500);
     }
 
-
+    //Method for if the user shakes the phone on the same question.
+    public void reShakeEvent() {
+        ImageView fadeImage = (ImageView)findViewById(R.id.imageView);
+        fadeImage.setImageResource(R.drawable.blank);
+        rollEvent();
+        shakeVibrationEvent();
+    }
 }
--- app/src/main/java/newton/magic8ball/MainActivity.java
+++ app/src/main/java/newton/magic8ball/MainActivity.java
@@ -22,36 +22,14 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     private ShakeDetector mShakeDetector;
-    /*private String[] response = {
-            "It is certain",
-            "It is decidedly so",
-            "Without a doubt",
-            "Yes definitely",
-            "You may rely on it",
-            "As I see it, yes",
-            "Most likely",
-            "Outlook good",
-            "Yes",
-            "Signs point to yes",
-            "Reply hazy, try again later",
-            "Ask again later",
-            "Better not tell you now",
-            "Cannot predict now",
-            "Concentrate and ask again",
-            "Don't count on it",
-            "My reply is no",
-            "My sources say no",
-            "Outlook not so good",
-            "Very doubtful"
-    };
-    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Setup for shake listening.
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mShakeDetector = new ShakeDetector();
@@ -63,12 +41,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Stops listening for shake event.  Saves battery.
     @Override
     protected void onPause() {
         mSensorManager.unregisterListener(mShakeDetector);
         super.onPause();
     }
 
+    //Resumes listening for shake event.
     @Override
     protected void onResume() {
         mSensorManager.registerListener(mShakeDetector,mAccelerometer,SensorManager.SENSOR_DELAY_UI);
@@ -76,14 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    //Starts the second activity after a successful shake.
     public void handleShakeEvent() {
         Intent intent = new Intent(this, BallScreenActivity.class);
         startActivity(intent);
-        //Toast.makeText(getApplicationContext(), response[random].toString(), Toast.LENGTH_LONG).show();
-        //Drawable answerTriangle = context.getResources().getDrawable(R.drawable.answer);
-        //Will need to incorperate a method that does the above to a different view than main_activity
-        //shakeVibrationEvent();
     }
     
 
